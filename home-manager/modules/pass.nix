15222503149c5165e57f9e0f71ebde1005563856
Kevin Barroso <kobouharriet@gmail.com>
2024-12-26 01:22:56 +0000

    Config GPG keys

--- home-manager/modules/pass.nix
+++ home-manager/modules/pass.nix
@@ -9,21 +9,41 @@
     };
   };
 
-  # Configuración de GPG
   programs.gpg = {
     enable = true;
+    settings = {
+      # Configuración de algoritmos y preferencias
+      "cert-digest-algo" = "SHA512";
+      "charset" = "utf-8";
+      "default-preference-list" = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+      "fixed-list-mode" = true;
+      "keyid-format" = "0xlong";
+      "list-options" = "show-uid-validity";
+      "no-comments" = true;
+      "no-emit-version" = true;
+      "no-symkey-cache" = true;
+      "personal-cipher-preferences" = "AES256 AES192 AES";
+      "personal-compress-preferences" = "ZLIB BZIP2 ZIP Uncompressed";
+      "personal-digest-preferences" = "SHA512 SHA384 SHA256";
+      "require-cross-certification" = true;
+      "s2k-cipher-algo" = "AES256";
+      "s2k-digest-algo" = "SHA512";
+      "use-agent" = true;
+      "verify-options" = "show-uid-validity";
+      "with-fingerprint" = true;
+    };
   };
 
-  # Configuración del agente GPG
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    defaultCacheTtl = 1800; # 30 minutos
-    maxCacheTtl = 7200; # 2 horas
+    defaultCacheTtl = 1800;
+    maxCacheTtl = 7200;
+    pinentryPackage = pkgs.pinentry-curses;
   };
 
-  # Paquetes relacionados
   home.packages = with pkgs; [
     gnupg
+    pinentry-curses
   ];
 }
